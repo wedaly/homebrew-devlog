960d9a9db78e2591f7961104fd142c44f3019817
Will Daly <will.e.daly@gmail.com>
2019-09-01 14:11:50 -0700

    Release version 1.0.0

--- Formula/devlog.rb
+++ Formula/devlog.rb
@@ -1,8 +1,8 @@
 class Devlog < Formula
   desc "Command-line tool for tracking your day-to-day software development work"
   homepage "https://devlog-cli.org"
-  url "https://github.com/wedaly/devlog/releases/download/0.3.0/devlog-0.3.0-x86_64-apple-darwin.tar.gz"
-  sha256 "104c35a184a2ee0fb8e5802a6e3580045e33c20cf85bce609c4c0a2087614a1e"
+  url "https://github.com/wedaly/devlog/releases/download/1.0.0/devlog-1.0.0-x86_64-apple-darwin.tar.gz"
+  sha256 "9fc8a906bf762a13360f5dae0b3536020c01329991a5c4802755611637143355"
 
   def install
     bin.install "devlog"
